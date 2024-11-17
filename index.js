c18e1698f09456cc60411a2501be9b1bc9abf861
Ariana <152944814+aam214@users.noreply.github.com>
2024-11-16 20:05:14 -0600

    Added a function of randomCode with variables to show a length of 6 characters by using a for loop and Math.random methods. Targeted color-number with querySelectorAll and named the variable allColorNumbers. Created new function of showColors to show the generated colors.

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-const mainContent = document.querySelector(".main-content");
+
+  const mainContent = document.querySelector(".main-content");
 
 for (let index = 0; index < 10; index++) {
   const colorNumber = document.createElement("div");
   colorNumber.classList.add("color-number");
   mainContent.appendChild(colorNumber);
 }
+
+const allColorNumbers = document.querySelectorAll("color-number");
+console.log(colorNumberBoxes);
+
+function showColors() {
+
+}
+randomCode();
+
+function randomCode() {
+  const characters = "0123456789abcdef";
+  const codeLength = 6;
+  let randomColor = "";
+  for (let index = 0; index < codeLength; index++) {
+  const randomNumber = Math.floor(Math.random() * characters.length);
+
+  randomColor += characters.substring(randomNumber, randomNumber +1);
+   console.log(randomColor);
+  }}
 });
